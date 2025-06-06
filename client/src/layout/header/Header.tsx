79f446dd6f5edc5889543611b6e2b2b86dc972da
Nolan.Nguyen <nolan.nguyen@avepoint.com>
2025-06-06 16:24:28 +0700

    [BOOKING-13] Responsive some module in Layout

--- client/src/layout/header/Header.tsx
+++ client/src/layout/header/Header.tsx
@@ -1,22 +1,54 @@
-import { Flex } from 'antd'
-import { FC } from 'react'
-import { Auth } from '../../pages/component/header/auth'
-import { LanguageSelector } from '../../pages/component/header/languageselector/LanguageSelector'
-import { Logo } from '../../pages/component/header/logo/Logo'
-import { Menu } from '../../pages/component/header/menu/Menu'
-import { SearchHeader } from '../../pages/component/header/search/Search'
-import "./Header.scss"
+import { MenuOutlined } from '@ant-design/icons';
+import { Button, Drawer, Flex, Grid } from 'antd';
+import { FC, useState } from 'react';
+import { Auth } from '../../pages/component/header/auth';
+import { LanguageSelector } from '../../pages/component/header/languageselector/LanguageSelector';
+import { Logo } from '../../pages/component/header/logo/Logo';
+import { Menu } from '../../pages/component/header/menu/Menu';
+import { SearchHeader } from '../../pages/component/header/search/Search';
+import './Header.scss';
 
 export const Header: FC = () => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const screens = Grid.useBreakpoint();
+
+  const toggleDrawer = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+  };
+
   return (
-    <Flex vertical={false} gap={8} justify='space-between' className='header'>
+    <Flex vertical={false} gap={8} justify="space-between" className="header">
       <Logo />
       <Menu />
-      <Flex gap={16}>
-        <SearchHeader />
-        <LanguageSelector />
-      </Flex>
-      <Auth />
+      {screens.md ? ( // Show full layout on larger screens (md: >768px)
+        <>
+          <Flex gap={16}>
+            <SearchHeader />
+            <LanguageSelector />
+          </Flex>
+          <Auth />
+        </>
+      ) : (
+        <>
+          <Flex gap={16} className="header--actions">
+            <SearchHeader />
+            <Button
+              className="menu-button"
+              icon={<MenuOutlined />}
+              onClick={toggleDrawer}
+            />
+          </Flex>
+          <Drawer
+            title="Menu"
+            placement="right"
+            onClose={toggleDrawer}
+            open={isDrawerOpen}
+          >
+            <LanguageSelector />
+            <Auth />
+          </Drawer>
+        </>
+      )}
     </Flex>
-  )
-}
+  );
+};
--- client/src/pages/component/footer/activeuser/ActiveUser.tsx
+++ client/src/pages/component/footer/activeuser/ActiveUser.tsx
@@ -1,7 +1,8 @@
-import { Flex, Skeleton, Statistic } from 'antd';
+import { Skeleton, Statistic } from 'antd';
 import { FC, useCallback, useEffect, useState } from 'react';
 import activeUserCountService from '~/api/content/activeusercount/activeUserCount';
 import { IActiveUserCount } from '~/api/types/activeusercount/IActiveUserCount';
+import { CardInfo } from '~/pages/common/footer/cardinfo/CardInfo';
 import "./ActiveUser.scss";
 
 export const ActiveUser: FC = () => {
@@ -21,13 +22,17 @@ export const ActiveUser: FC = () => {
       })
   }, [])
 
-  if (isLoading) {
-    return <Skeleton.Input active={true} size="small" block={false} />
+  const renderActiveUserCount = () => {
+    if (isLoading) {
+      return <Skeleton.Input active={true} size="small" block={false} />
+    }
+    return <Statistic valueStyle={{ color: '#fff' }} value={activeUser?.count} />
   }
 
   return (
-    <Flex className='active-user-count'>
-      <Statistic valueStyle={{ color: '#fff' }} title="Active Users" value={activeUser?.count} />
-    </Flex>
+    <CardInfo
+      title={'Visitor Count'}
+      chidren={renderActiveUserCount()}
+    />
   )
 }
